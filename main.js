b16ef663bac2cb9e6ef129ca71f9f1c38b176d78
Horacio <horaciobriceo2214@gmail.com>
2024-06-24 21:46:07 -0600

    brrr'nt

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
+  <h1>
+  HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
@@ -19,6 +21,6 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
